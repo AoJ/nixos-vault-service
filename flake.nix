937950ecb0ac4c42c6d562e430f7cbb5ed2bc1ae
AoJ <aoj@n13.cz>
2024-07-10 13:21:44 +0200

    update nixpkgs to 0.2405.631440.tar.gz

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS integration for Vault-backed secrets on systemd services.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.631440.tar.gz";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.0.1.tar.gz";
   };
 
